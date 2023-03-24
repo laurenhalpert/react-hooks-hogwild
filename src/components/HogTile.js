6eda4766857b8c01c24bacfabd3dbeef345b4433
Lauren <halpertln@gmail.com>
2023-03-24 15:18:18 -0600

    proper ui style for images

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import DisplayDetails from "./DisplayDetails.js"
 
 
+
 function HogTile({ onHide, name, image, specialty, greased, weight, highestMedalAchieved }) {
     const [isClicked, setIsClicked] = useState(false)
     function handleClick() {
@@ -11,7 +12,7 @@ function HogTile({ onHide, name, image, specialty, greased, weight, highestMedal
     return(
         <div className="ui eight wide column" onClick={handleClick} >
             <p>{name}</p>
-            <img src={image} alt="pig"></img>
+            <img src={image} alt="pig" className="ui image"></img>
             {isClicked? <DisplayDetails specialty={specialty} greased={greased} weight={weight} highestMedalAchieved={highestMedalAchieved} /> : null}
             <button onClick={onHide}>Hide Pig</button>
         </div>
